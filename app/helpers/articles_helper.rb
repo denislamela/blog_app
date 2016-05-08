e20c51df70804d87482fb87b606f06ec0aa30a6b
Denís Lamela <denislamela@hotmail.com>
2016-05-08 15:39:22 +0200

    Russian doll caching in welcome page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+    def cache_key_for_articles
+      count          = Article.count
+      max_updated_at = Article.maximum(:updated_at).try(:utc).try(:to_s, :number)
+      "articles/all-#{count}-#{max_updated_at}"
+    end
 end
